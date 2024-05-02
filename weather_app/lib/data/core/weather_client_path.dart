634268c41f210b0b7f54740519b7c5ddfb65637f
Natalia Prokopiuk <nat.prokopiuk@gmail.com>
2024-05-02 22:01:22 +0200

    Completed WeatherClientPath class

--- weather_app/lib/data/core/weather_client_path.dart
+++ weather_app/lib/data/core/weather_client_path.dart
@@ -1,2 +1,43 @@
 import 'package:weather_app/data/core/credentials.dart';
 
+enum WeatherClientPath {
+  current,
+  forecast,
+  history;
+
+  String get path {
+    switch (this) {
+      case WeatherClientPath.current:
+        return '/v1/current.json';
+      case WeatherClientPath.forecast:
+        return '/v1/forecast.json';
+      case WeatherClientPath.history:
+        return '/v1/history.json';
+    }
+  }
+
+  Uri get baseUri => Uri.https(
+    'api.weatherapi.com',
+    '',
+    <String, dynamic>{
+      'key': Keys.weatherApiKey,
+      'aqi': 'yes',
+    },
+  );
+
+  Uri getUri([String? query]) {
+    if (query == null) {
+      return Uri.https(
+        baseUri.authority,
+        path,
+        baseUri.queryParameters,
+      );
+    }
+
+    return Uri.https(
+      baseUri.authority,
+      WeatherClientPath.current.path,
+      <String, dynamic>{...baseUri.queryParameters, 'q': query},
+    );
+  }
+}
